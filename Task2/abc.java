1d111e56275efb6fa2fbe0c65ab9af77d6ecde37
Ishan Khan <khaneshan166@gmail.com>
2024-07-24 13:08:25 +0530

    Add files via upload

---
+++ Task2/abc.java
@@ -0,0 +1,12 @@
+public class abc {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int count =0;
+        for(int i=0;i<10;i++){
+            count++;
+            if(count==5){
+                System.out.println("Hello");
+            }
+        }
+    }
+}
